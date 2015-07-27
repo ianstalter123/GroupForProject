6b59494477eaac06e094bd763b8faa0d822b3538
Madeline <madelinebagby@yahoo.com>
2015-07-27 11:03:52 -0700

    Added gem and model dependencies

--- news_bot/app/models/user.rb
+++ news_bot/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 	validates :password, length: {minimum: 8, maximum: 20}
 
-
+	has_many :articles
 end
