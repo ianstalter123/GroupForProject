87795cd6b441cef9f3f1ad9e2e733d2ede674fc5
TonyNguyen101 <anthonylenguyen04@gmail.com>
2015-07-27 11:09:47 -0700

    nothing new

--- news_bot/app/models/user.rb
+++ news_bot/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 	validates :password, length: {minimum: 8, maximum: 20}
+
+	has_many :articles
 end
