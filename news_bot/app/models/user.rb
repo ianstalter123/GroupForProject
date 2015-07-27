48b9947d8428f2669ddc092c1c9b94ff240b67bb
TonyNguyen101 <anthonylenguyen04@gmail.com>
2015-07-27 11:06:16 -0700

    nothing new

--- news_bot/app/models/user.rb
+++ news_bot/app/models/user.rb
@@ -3,5 +3,4 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 	validates :password, length: {minimum: 8, maximum: 20}
 
-
 end
