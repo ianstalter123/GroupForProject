5d4e1abcb35a3e34ba8c860c05f17e2031cecf4b
Madeline <madelinebagby@yahoo.com>
2015-07-27 10:48:24 -0700

    Added testing to include email validations with formatting

--- news_bot/app/models/user.rb
+++ news_bot/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	validates :email, presence: true
-	validates :password, length: {minimum: 8}
+	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+	validates :password, length: {minimum: 8, maximum: 20}
+
 
 end
--- news_bot/spec/models/user_spec.rb
+++ news_bot/spec/models/user_spec.rb
@@ -18,9 +18,39 @@ describe User do
 	describe "when email is present" do
 		it "should be valid" do
 			user = User.create(
-				email: "someone",
+				email: "someone@gmail.com",
 				password: "something",
 				password_confirmation: "something")
+			expect(user).to be_valid
+		end		
+	end
+
+	describe "when email does not have an @ sign" do
+		it "should not be valid" do
+			user = User.create(
+				email: "someonegmailcom",
+				password: "something",
+				password_confirmation: "something")
+			expect(user).to_not be_valid
+		end		
+	end
+
+	describe "when email has an @ sign" do
+		it "should be valid" do
+			user = User.create(
+				email: "someone@gmail.com",
+				password: "something",
+				password_confirmation: "something")
+			expect(user).to be_valid
+		end		
+	end
+
+	describe "when password is between 8 and 20 characters" do
+		it "should be valid" do
+			user = User.create(
+				email: "someone@blah.edu",
+				password: "validpassword",
+				password_confirmation: "validpassword")
 			expect(user).to be_valid
 		end		
 	end
@@ -28,12 +58,44 @@ describe User do
 	describe "when the password is less than 8 characters" do
 		it "should not be valid" do
 			user = User.create(
-				email: "someone",
+				email: "someone@gmail.com",
 				password: "2short",
 				password_confirmation: "2short")
 			expect(user).to_not be_valid
 		end		
 	end
+
+	describe "when the password is more than 20 characters" do
+		it "should not be valid" do
+			user = User.create(
+				email: "someone@gmail.com",
+				password: "thispasswordisgoingtobetoolong",
+				password_confirmation: "thispasswordisgoingtobetoolong")
+			expect(user).to_not be_valid
+		end		
+	end
+
+	describe "when password and password_confirmation are not the same" do
+		it "should not be valid" do
+			user = User.create(
+				email: "someone@gmail.com",
+				password: "thisdoesntmatch",
+				password_confirmation: "thisone")
+			expect(user).to_not be_valid
+		end		
+	end
+
+	describe "when password and password_confirmation are the same" do
+		it "should be valid" do
+			user = User.create(
+				email: "someone@gmail.com",
+				password: "thismatches",
+				password_confirmation: "thismatches")
+			expect(user).to be_valid
+		end		
+	end
+
+
 
 end
 
