7236c6ed74e65cdfa9e89ead178d94fffe6f0bbd
Madeline <madelinebagby@yahoo.com>
2015-07-27 13:45:23 -0700

    Added signup and login functionality

--- news_bot/app/controllers/sessions_controller.rb
+++ news_bot/app/controllers/sessions_controller.rb
@@ -3,5 +3,32 @@ class SessionsController < ApplicationController
   end
 
   def signup
+  	@user = User.new
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_articles_path(@user), flash: {success: "Created!"}
+    else
+      redirect_to signup_path, flash: {error: @user.errors.full_messages}
+	end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to login_path
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
+
+
+
