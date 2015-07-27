5833ff5420b080324580f5abd3cde2e7184522ef
ianstalter123 <istalter66@gmail.com>
2015-07-27 15:45:03 -0700

    article views and forms up

--- news_bot/app/controllers/articles_controller.rb
+++ news_bot/app/controllers/articles_controller.rb
@@ -1,10 +1,33 @@
 class ArticlesController < ApplicationController
   def index
+  	@articles = Article.all
   end
 
   def show
+  	@article = Article.find_by_id(params[:id])
+  end
+   def new
+   	@user = User.find_by_id(session[:user_id])
+  	@article = Article.new 
+  end
+   def create
+
+   	 @article = Article.create article_params
+    if @article.save
+      
+      redirect_to user_articles_path(session[:user_id]), flash: {success: "Created!"}
+    else
+      redirect_to new_user_article_path, flash: {error: @article.errors.full_messages}
+	end
+  	
   end
 
   def compare
+  end
+
+  
+  private
+  def article_params
+    params.require(:article).permit(:url, :title)
   end
 end
