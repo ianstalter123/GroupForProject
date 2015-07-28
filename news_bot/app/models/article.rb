91c336a9650b1f2df0620ec1f2a10ea1b61a327b
Madeline <madelinebagby@yahoo.com>
2015-07-28 10:34:03 -0700

    Added scraping for Fox, CNN, BBC, and Huffington Post

--- news_bot/app/models/article.rb
+++ news_bot/app/models/article.rb
@@ -9,7 +9,10 @@ class Article < ActiveRecord::Base
           if self.source == "Huffington Post"
                text = raw_html.css('.entry-component__content p').text
                author = raw_html.css('.author-component__name').text
-               self.update(raw_html: raw_html, text: text,author: author)
+               title = raw_html.css('.entry-component__headline').text
+               date =raw_html.css('.entry-component__posted').text
+               image = raw_html.css(".wrapper-component .image-component img")[0].attr("src")
+               self.update(raw_html: raw_html, text: text,author: author, title: title, date_published: date, image: image)
           elsif self.source == "Fox"
                text = raw_html.css("div[itemprop='articleBody']").text 
                title = raw_html.css("h1[itemprop='headline']").text 
@@ -22,12 +25,13 @@ class Article < ActiveRecord::Base
                title = raw_html.css("h1[class='pg-headline']").text 
                date = raw_html.css("p[class='update-time']").text 
                author = raw_html.css("span[class='metadata__byline__author']").text 
-               self.update(raw_html: raw_html, text: text,author: author, title: title, date_published: date)
+               self.update(raw_html: raw_html, text: text,author: author, title: title, date_published: date, image: image)
           elsif self.source == "BBC News"
                title = raw_html.css('title').text
                text = raw_html.css('div.story-body__inner p').text
                date = raw_html.css("div.date").attr('data-datetime').value
                author = raw_html.css("meta[property = 'og:article:author']").attr('content').value
+               image = raw_html.css('figure.lead img').attr('src').value
                self.update(raw_html: raw_html, text: text,author: author, title: title, date_published: date, image: image)
           end	
 	 end
