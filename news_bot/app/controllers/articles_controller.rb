80adad1d85a4d7e56e1a3518b0f6a139e78c5182
TonyNguyen101 <anthonylenguyen04@gmail.com>
2015-07-28 15:10:52 -0700

    added env, alchemy gem with key

--- news_bot/app/controllers/articles_controller.rb
+++ news_bot/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+ 
+
   def index
   	@articles = Article.all
   end
@@ -6,17 +8,25 @@ class ArticlesController < ApplicationController
   def show
   	@article = Article.find_by_id(params[:id])
   end
-   def new
+  
+  def new
    	@user = User.find_by_id(session[:user_id])
   	@article = Article.new 
   end
-   def create
+  
+  def create
     
-   	 @article = Article.create article_params
+   	@article = Article.create article_params
 
     if @article.save
-
       @article.save_content
+      AlchemyAPI.key = ENV['ALCHEMY_KEY']
+      results = AlchemyAPI::KeywordExtraction.new.search(url: 'http://www.alchemyapi.com/')
+      # alchemyapi = AlchemyAPI.new()
+      # results = AlchemyAPI
+
+      binding.pry
+
       redirect_to user_articles_path(session[:user_id]), flash: {success: "Created!"}
     else
       redirect_to new_user_article_path, flash: {error: @article.errors.full_messages}
